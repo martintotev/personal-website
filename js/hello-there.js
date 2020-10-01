ab17536d219da5262f4d0f770ceab7f5272483f0
Martin Totev <martintotev89@gmail.com>
2020-10-01 19:59:48 +0900

    new-website
    Now it's time to add to it

---
+++ js/hello-there.js
@@ -0,0 +1,19 @@
+//Put link to site where I referenced this short JS code
+
+var today = new Date();
+var hourNow = today.getHours();
+var greet;
+
+if (hourNow > 18) {
+    greet = 'Good evening';
+} else if (hourNow > 12) {
+    greet = 'Good afternoon';
+} else if (hourNow > 5) {
+    greet = 'Good morning';
+} else {
+    greet = 'Good day';
+}
+
+$(document).ready(function () {
+    $('#greet').html(greet);
+});
