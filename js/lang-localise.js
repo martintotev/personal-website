78a43fef3b5bc63c067fdb502971e5f4379c7248
Martin Totev <martintotev89@gmail.com>
2021-01-25 23:26:02 +0900

    small fixes and update to js scripts
    I did fixed some typos and fixed my language localisation script that should display a hi in either english (several versions), koeran or bulgarian.

--- js/lang-localise.js
+++ js/lang-localise.js
@@ -1,13 +1,17 @@
-// Localise greeting based on the browswer's language
+// Localise greeting based on the browser's language
 
 var userLang = navigator.language || navigator.userLanguage; 
 console.log('The language code is: ' + userLang);
 console.log(typeof userLang);
 
-if ((userLang == 'en-GB') || (userLang == 'en-CA') || (userLang == 'en-US') || (userLang == 'en-ZA') || (userLang == 'en-IE')) {
+if ((userLang == 'en-GB') || (userLang == 'en-US') || (userLang == 'en-ZA') || (userLang == 'en-IE')) {
     langLocal = 'hello there!';
 } else if ((userLang == 'en-AU') || (userLang == 'en-NZ')) {
     langLocal = 'gday mate!';
+} else if ((userLang == 'en-CA')) {
+    langLocal = 'Hello and Salut, eh?';
+} else if ((userLang == 'fr-FR') || (userLang == 'fr-CA') || (userLang == 'fr-BE') || (userLang == 'fr-LU')) {
+    langLocal = 'Bonjour!';
 } else if (userLang == 'ko') {
     langLocal = '안녕하세요?';
 } else if (userLang == 'bg') {
